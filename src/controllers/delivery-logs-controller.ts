576f503547c9bfce111c05c1c2d615ff06b33001
Fabio Delong <frdelong@gmail.com>
2025-08-02 00:31:51 -0300

    feat: enhance delivery logs controller with show method and validation

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -7,31 +7,52 @@ class DeliveryLogsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       delivery_id: z.string().uuid(),
-      description: z.string()
+      description: z.string(),
     })
-    
+
     const { delivery_id, description } = bodySchema.parse(request.body)
 
     const delivery = await prisma.delivery.findUnique({
-      where: { id: delivery_id }
+      where: { id: delivery_id },
     })
 
     if (!delivery) {
       throw new AppError("delivery not found", 404)
     }
-    
-    if(delivery.status === "processing"){
+
+    if (delivery.status === "processing") {
       throw new AppError("change status to shipped", 404)
     }
 
     await prisma.deliveryLog.create({
       data: {
         deliveryId: delivery_id,
-        description
-      }
+        description,
+      },
     })
 
     return response.status(201).json()
+  }
+
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      delivery_id: z.string().uuid(),
+    })
+
+    const { delivery_id } = paramsSchema.parse(request.params)
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    })
+
+    if (
+      request.user?.role === "customer" && 
+      request.user.id !== delivery?.userId
+    ) {
+      throw new AppError("the user can only view their own deliveries", 401)
+    }
+
+    return response.json(delivery)
   }
 }
 
